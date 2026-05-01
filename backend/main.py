d27b3e6a8b00fd6097ab6d47f6921d2aca9aa624
Tuong Nguyen Pham <96371265+Currybon30@users.noreply.github.com>
2026-04-30 20:15:57 -0400

    Update main.py - ready for deployment

--- backend/main.py
+++ backend/main.py
@@ -17,4 +17,4 @@ app.include_router(chatbot_router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
