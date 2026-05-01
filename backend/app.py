198b3814720d859fa0efed59b8cbdd68dbe92297
Tuong Nguyen Pham <96371265+Currybon30@users.noreply.github.com>
2026-05-01 10:09:08 -0400

    Test deployment

--- backend/app.py
+++ backend/app.py
@@ -3,12 +3,12 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from fastapi.middleware.cors import CORSMiddleware
 import dotenv
 import os
-from views.faq_saving import router as faq_saving_router
-from views.pdf_saving import router as pdf_saving_router
-from views.login import router as login_router
-from views.signup import router as signup_router
-from views.manage_accounts import router as manage_accounts_router
-from views.security import router as security_router
+# from views.faq_saving import router as faq_saving_router
+# from views.pdf_saving import router as pdf_saving_router
+# from views.login import router as login_router
+# from views.signup import router as signup_router
+# from views.manage_accounts import router as manage_accounts_router
+# from views.security import router as security_router
 # from views.chatbot import router as chatbot_router
 
 
@@ -24,12 +24,12 @@ app.add_middleware(
 )
 
 
-app.include_router(faq_saving_router)
-app.include_router(pdf_saving_router)
-app.include_router(login_router)
-app.include_router(signup_router)
-app.include_router(manage_accounts_router)
-app.include_router(security_router)
+# app.include_router(faq_saving_router)
+# app.include_router(pdf_saving_router)
+# app.include_router(login_router)
+# app.include_router(signup_router)
+# app.include_router(manage_accounts_router)
+# app.include_router(security_router)
 # app.include_router(chatbot_router)
 
 MONGO_URI = os.getenv('MONGO_URI')
