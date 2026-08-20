e35c06e69eff484ea99880841095985925591571
Tuong Nguyen Pham <tuongnguyen2004danang@gmail.com>
2026-08-20 08:53:15 -0400

    Update admin_api_validation.py
    Modify the name of the argument

--- backend/auth/admin_api_validation.py
+++ backend/auth/admin_api_validation.py
@@ -5,7 +5,8 @@ import os
 dotenv.load_dotenv()
 ADMIN_API_KEY = os.getenv('ADMIN_API_KEY')
 
-def validate_admin_api_key(admin_api_key: str = Header(None)):
-    if admin_api_key is None or admin_api_key != ADMIN_API_KEY:
+def validate_admin_api_key(x_api_key: str = Header(None)):
+    print(f"Validating admin API key: {x_api_key}")
+    if x_api_key is None or x_api_key != ADMIN_API_KEY:
         return False
     return True
